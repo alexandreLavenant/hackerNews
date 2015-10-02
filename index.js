2daa4696ee8e8465b20d6c98506abf9e9d3dd85a
Alexandre Lavenant <alexandre.lavenant@outlook.com>
2015-10-02 12:06:04 +0200

    function result is a callback

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var hacker = require('./lib/hackerLib');
+/* jshint laxcomma : true */
+var hacker = require('./lib/hackerLib')
+	, open   = require('open')
+	;
 
-hacker.news(100, 10);
+hacker.news(100, 5, function(err, articles)
+{
+	for(var i=0, l=articles.length; i<l; ++i)
+	{
+		open(articles[i].link);
+	}
+});
